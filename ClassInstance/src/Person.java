ee07afb95e3c677de605a57267d3b4bfaddcbfe0
takashi1006 <takashi1006@live.jp>
2019-11-23 17:32:25 +0900

    ### クラスとインスタンス * 課題提出

---
+++ ClassInstance/src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    String name = "";
+    int age = 0;
+
+    public void showMyProfile() {
+        System.out.println("私の名前は" + this.name + "です。");
+        System.out.println("年齢は" + this.age + "です。");
+        System.out.println("よろしくお願いします！");
+    }
+}
---
+++ ClassInstance/src/task2.java
@@ -0,0 +1,11 @@
+
+public class task2 {
+
+    public static void main(final String[] args) {
+        Person person = new Person();
+        person.name = "テスト ホゲ";
+        person.age = 36;
+        person.showMyProfile();
+    }
+
+}
