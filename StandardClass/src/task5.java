312b52813595fefe1cc5e5268a1f9fedbacdb1a0
takashi1006 <takashi1006@live.jp>
2019-11-25 14:31:40 +0900

    * 課題提出

--- StandardClass/src/task5.java
+++ StandardClass/src/task5.java
@@ -6,17 +6,18 @@ public class task5 {
         System.out.println("元の文字列： " + message);
         String[] messageArray = message.split("_");
         String answerString = "";
+        int answerNumber = 0;
         for (int i = 0; i < messageArray.length; i++) {
             System.out.println("文字列(配列)： " + messageArray[i]);
             String searchString = messageArray[i];
-            int answerNumber = 0;
+            int number = 0;
             for (int j = 0; j < messageArray.length; j++) {
-                int number = 0;
                 if ( i != j && messageArray[j].equals(searchString)) {
                     number++;
-                }
-                if (number > answerNumber) {
-                    answerString = messageArray[i];
+                    if (number > answerNumber) {
+                        answerNumber = number;
+                        answerString = searchString;
+                    }
                 }
             }
         }
